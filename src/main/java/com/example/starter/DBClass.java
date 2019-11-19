18de30e27794a5acb6d886929dbdc726da9bd5c2
Pragya Saxena <pragya.saxena@unisys.com>
2019-11-19 18:43:20 +0530

    update

---
+++ src/main/java/com/example/starter/DBClass.java
@@ -0,0 +1,97 @@
+package com.example.starter;
+
+import java.nio.charset.Charset;
+import java.sql.Connection;
+
+//import org.json.JSONException;
+import io.vertx.core.json.JsonObject;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Random;
+
+
+public class DBClass {
+	private static Connection dbconnection;
+	public DBClass() {
+		try {
+			dbconnection = DriverManager.getConnection("jdbc:mysql://129.221.48.135:3307/userdb", "root", "something");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}  
+public JsonObject registerUser(String name, String email, String password) {
+	JsonObject obj = null;
+	try {
+		
+	    
+		StringBuilder sqlquery = new StringBuilder();
+		sqlquery.append("INSERT INTO userdb.user(username,userid,emailid,password) VALUES('");
+		sqlquery.append(name+"','"+email+"','"+email+"','"+password+"')");
+		System.out.println(sqlquery.toString());
+		PreparedStatement ps = dbconnection.prepareStatement(sqlquery.toString());
+		ResultSet rs = ps.executeQuery();
+		JsonObject tokenObj = new JsonObject();
+		tokenObj.put("name", name)
+				.put("email", email)
+				.put("userid", email); 
+		//TODO: generate userID
+		obj = new JsonObject();
+		obj.put("Token", tokenObj);
+		
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	return obj;
+}
+
+public JsonObject authenticateUser(String email, String password) {
+	JsonObject obj = null;
+	try {
+		PreparedStatement ps = dbconnection.prepareStatement("select  * from userdb.user");
+		ResultSet rs = ps.executeQuery();
+		while (rs.next()) {
+			String emailid = rs.getString("emailid");
+			String userPwd = rs.getString("password");
+			String userName = rs.getString("username");
+			String userID = rs.getString("userid");
+			
+			System.out.println(rs);
+			if(emailid.equals(email)  && userPwd.equals(password)) {
+				JsonObject tokenObj = new JsonObject();
+				tokenObj.put("name", userName)
+				.put("email", emailid)
+				.put("userid", userID); 
+				//TODO: generate userID
+				obj = new JsonObject();
+				obj.put("Token", tokenObj);
+				break;
+			}
+		}
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	return obj;
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		DBClass db = new DBClass();
+		JsonObject obj = db.authenticateUser("pramodh.email", "pramodh");
+		System.out.println(obj.toString());
+		JsonObject obj1 = db.registerUser("Ratna1", "ratna.email1", "ratna.password");
+		System.out.println(obj1.toString());
+		
+	}
+
+}
--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -1,23 +1,139 @@
 package com.example.starter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Promise;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.CorsHandler;
+import com.example.starter.DBClass;
 
-public class MainVerticle extends AbstractVerticle {
-
+public class MainVerticle extends AbstractVerticle{
+	DBClass db = new DBClass();
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(4020, http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
-    });
+	  
+	  
+	  Router router = Router.router(this.vertx);
+		
+		//to give CORS permissions 
+		Set<String> allowedHeaders = new HashSet<>();
+		allowedHeaders.add("x-requested-with");
+		allowedHeaders.add("Access-Control-Allow-Origin");
+		allowedHeaders.add("origin");
+		allowedHeaders.add("Content-Type");
+		allowedHeaders.add("accept");
+
+		Set<HttpMethod> allowedMethods = new HashSet<>();
+		allowedMethods.add(HttpMethod.POST);
+		allowedMethods.add(HttpMethod.GET);
+		allowedMethods.add(HttpMethod.DELETE);
+		
+		router.route().handler(CorsHandler.create("*")
+			.allowedHeaders(allowedHeaders)
+          .allowedMethods(allowedMethods));
+		
+		router.route().handler(BodyHandler.create());
+		//call to different post and get api
+		router.post("/user").handler(this::signUp);
+		router.post("/user/signin").handler(this::signIn);
+//		router.get("/user").handler(this::getUsers);
+//		router.delete("/user").handler(this::deleteUser);
+			
+  	String host = "IN-SaxenaPr-1";
+  	int port = 8080;
+      HttpServerOptions op = new HttpServerOptions();
+
+  	op.setHost(host);
+ 		vertx
+ 			.createHttpServer(op)
+ 			.requestHandler(router)
+ 			.listen(port);
+     // Deploying all the verticles 
+		
+	}
+
+  
+  private void signUp(RoutingContext routingContext) {
+		JsonObject userObject = routingContext.getBodyAsJson();
+		//calling jdbsCLient
+		String name = userObject.getString("name");
+		String email = userObject.getString("email");
+		String password = userObject.getString("password");
+		JsonObject regObj = db.registerUser(name,email,password);
+		if(regObj!=null) {
+			routingContext.response()
+				.setStatusCode(201)
+				.putHeader("content-type", "application/json")
+				.end(regObj.toString());
+		}
+		else {
+			routingContext.response()
+				.setStatusCode(500)
+				.end();
+			}
+		
+	}
+
+  private void signIn(RoutingContext routingContext) {
+	JsonObject login = routingContext.getBodyAsJson();
+	String email = login.getString("email");
+	String password = login.getString("password");
+	JsonObject user = db.authenticateUser(email, password);
+	//calling sigin jdbc
+	if(user!=null) {
+		routingContext.response()
+			.setStatusCode(200)
+			.putHeader("content-type", "application/json")
+			.end(user.toString());
+	}
+			else {
+				routingContext.response()
+					.setStatusCode(403)
+					.end();
+			}
+	  
   }
+//
+//
+//private void getUsers(RoutingContext routingContext) {
+//	String token = routingContext.request().getParam("")
+//	String mail = routingContext.request().getParam("mail");
+//	System.out.println("mail:: " + mail);
+//	JsonObject request = new JsonObject().put("mail", mail);
+//	EventBus eb = vertx.eventBus();
+//	//communicating with customerDetailVerticle
+//	eb.send("CustomerDetailsPage", request, replyHandler -> {
+//		if(replyHandler.succeeded()) {
+//			routingContext.response()
+//				.putHeader("content-type", "application/json")
+//				.end(replyHandler.result().body().toString());
+//		}
+//	});
+//}
+//  private void deleteUser(RoutingContext routingContext) {
+//	  JsonObject user = routingContext.getBodyAsJson();
+//	  //some code with if
+//	  
+//	  if() {
+//		  routingContext.response()
+//		  	.setStatusCode(200)
+//		  	.end();
+//	  }
+//	  else {
+//		  routingContext.response()
+//		  	.setStatusCode(403)
+//		  	.end();
+//	  }
+//  }
+  
+  
 }
